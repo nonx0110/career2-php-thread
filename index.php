22747a705b932e10e0ce7da117c07c886d622c65
2019 DSP6101 <2019DSP6101@ntsv6101.com>
2020-07-01 11:45:52 +0900

    new

--- index.php
+++ index.php
@@ -1,101 +1,87 @@
-<html>
-<head><title>掲示板</title></head>
+<?php
+session_start();
+
+function setToken()
+{
+    $token = sha1(uniqid(mt_rand(), true));
+    $_SESSION['token'] = $token;
+}
+
+function checkToken()
+{
+    if (empty($_SESSION['token'])) {
+        echo "Sessionが空です";
+        exit;
+    }
+
+    if (($_SESSION['token']) !== $_POST['token']) {
+        echo "不正な投稿です。";
+        exit;
+    }
+
+    $_SESSION['token'] = null;
+}
+
+if (empty($_SESSION['token'])) {
+    setToken();
+}
+?>
+
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link crossorigin="anonymous" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" rel="stylesheet">
+    <title>掲示板App</title>
+</head>
 <body>
-<font color="red">
-    <center>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-        <h1>掲示板App</h1>
-    </center>
-</font>
-<h2>投稿フォーム</h2>
 
-<form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
-    <input type="text" name="personal_name" placeholder="名前" required><br><br>
-    <textarea name="contents" rows="8" cols="40" placeholder="内容" required>
-</textarea><br><br>
-    <input type="submit" name="btn" value="投稿する">
-    
-</form>
+<div class="container">
+    <div class="col-md-8">
+        <h1 class="text-center text-primary py-3">掲示板App</h1>
 
-<h2>スレッド</h2>
+        <h2 class="text-muted py-3">投稿フォーム</h2>
+        <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
+            <input type="text" class="form-control" name="personal_name" placeholder="名前" required><br><br>
+            <textarea name="contents" class="form-control" rows="8" cols="40" placeholder="内容" required></textarea>
+            <br><br>
+            <input type="hidden" name="token" value="<?php echo $_SESSION['token']; ?>">
+            <input class="btn btn-primary"  type="submit" name="btn" value="投稿する">
+        </form>
 
-<form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
-    <input type="hidden" name="method" value="DELETE">
-    <button type="submit">スレッドの全削除</button>
-</form>
 
+        <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
+            <input type="hidden" name="method" value="DELETE">
+            <button class="btn btn-danger" type="submit">投稿を全削除する</button>
+        </form>
+
+        <h2 class="text-muted py-3">スレッド</h2>
 <?php
 
+date_default_timezone_set('Asia/Tokyo');
 const THREAD_FILE = 'thread.txt';
 
-function threadInit(){
-    file_put_contents(THREAD_FILE,"");
-    //echo '実行';
-}
+require_once './Thread.php';
+$thread = new Thread('掲示板App');
 
-function readData() {
-    // ファイルが存在しなければデフォルト空文字のファイルを作成する
-    if (! file_exists(THREAD_FILE)) {
-        $fp = fopen(THREAD_FILE, 'w');
-        fwrite($fp, '');
-        fclose($fp);
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (isset($_POST["method"]) && $_POST["method"] === "DELETE") {
+        $thread->delete();
+    } else {
+        $thread->post($_POST['personal_name'], $_POST['contents']);
     }
-
-    $thread_text = file_get_contents(THREAD_FILE);
-    //echo '実行';
-    echo $thread_text;
-}
-
-function writeData() {
-    $personal_name = $_POST['personal_name'];
-    $contents = $_POST['contents'];
-    $contents = nl2br($contents);
-    $day = date("Y/m/d");
-    $time = date(" H:i:s");
-    $week_Data = date("w");
-    $week = ["日","月","火","水","木","土"];
-
-    $data = "<hr>\n";
-    $data = $data."<p>投稿者:".$personal_name.":".$day."(".$week[$week_Data].")".$time."</p>\n";
-    $data = $data."<p>内容:</p>\n";
-    $data = $data."<p>".$contents."</p>\n";
-
-    $fp = fopen(THREAD_FILE, 'a');
-
-    if ($fp){
-        if (flock($fp, LOCK_EX)){
-            if (fwrite($fp,  $data) === FALSE){
-                print('ファイル書き込みに失敗しました');
-            }
-
-            flock($fp, LOCK_UN);
-        }else{
-            print('ファイルロックに失敗しました');
-        }
-
-    }
-
-    fclose($fp);
 
     // ブラウザのリロード対策
-    // 11:06
     $redirect_url = $_SERVER['HTTP_REFERER'];
     header("Location: $redirect_url");
     exit;
-    // sample commen
 }
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    if(isset($_POST["method"]) && $_POST["method"] === "DELETE"){
-        threadInit();
-    }else{
-        writeData();
-    }
-}
-
-readData();
+echo $thread->getList();
 
 ?>
+
 <!-- JS, Popper.js, and jQuery -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
