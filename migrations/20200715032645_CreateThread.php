441553ed0ae9e696aaae35c7f2dd2b68b0a46006
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-07-15 11:02:14 +0900

    追加

--- migrations/20200715032645_CreateThread.php
+++ migrations/20200715032645_CreateThread.php
@@ -9,7 +9,18 @@ class CreateThread extends Migration
      */
     public function up()
     {
-
+        $sql = <<<EOT
+CREATE TABLE IF NOT EXISTS `thread` (
+`id` int(11) AUTO_INCREMENT,
+`name` varchar(255) NOT NULL,
+`content` varchar(255) NOT NULL,
+`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+`deleted_at` DATETIME DEFAULT NULL,
+PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8;
+EOT;
+        $container = $this->getContainer();
+        $container['db']->query($sql);
     }
 
     /**
@@ -17,6 +28,8 @@ class CreateThread extends Migration
      */
     public function down()
     {
-
+        $sql = "DROP TABLE `thread`;";
+        $container = $this->getContainer();
+        $container['db']->query($sql);
     }
 }
